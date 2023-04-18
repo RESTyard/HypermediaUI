c314ffe1e62465998b894c4ece3d66fafe7acbad
Pratik Poudel <poudel@data-cybernetics.com>
2023-04-18 12:38:25 +0200

    properly align mat-icon in button, css updates #853

--- src/app/hypermedia-view/actions-view/parameter-action/formly-types/array-type.ts
+++ src/app/hypermedia-view/actions-view/parameter-action/formly-types/array-type.ts
@@ -5,30 +5,43 @@ import {style} from '@angular/animations';
 @Component({selector: 'formly-array-type',
   template: `
     <div>
-      <legend *ngIf="props.label">{{ props.label }}</legend>
-      <p *ngIf="props.description">{{ props.description }}</p>
-      <div class="d-flex flex-row-reverse" style="margin-bottom: 3px;">
-        <button type="button" (click)="add()" mat-flat-button>
-          <mat-icon id="addHeaderButton">add</mat-icon>
-        </button>
-      </div>
-      <div role="alert" *ngIf="showError && formControl.errors">
-        <formly-validation-message [field]="field"></formly-validation-message>
-      </div>
-      <div *ngFor="let field of field.fieldGroup; let i = index" class="form-field-container">
-        <div class="col-10">
-          <formly-field [field]="field"></formly-field>
+      <mat-card style="margin: 10px 0 0 0" appearance="raised">
+        <div class="row">
+          <mat-card-header>
+            <legend *ngIf="props.label">{{ props.label }}</legend>
+          </mat-card-header>
+          <mat-card-actions>
+            <div style="margin-bottom: 3px;">
+              <button type="button" (click)="add()" mat-flat-button>
+                <mat-icon id="addHeaderButton">add</mat-icon>
+              </button>
+            </div>
+          </mat-card-actions>
+          <mat-card-header>
+            <p *ngIf="props.description">{{ props.description }}</p>
+          </mat-card-header>
         </div>
-        <div *ngIf="field.props['removable'] !== false" class="col-2">
-          <button type="button" (click)="remove(i)" mat-flat-button>
-            <mat-icon class="deleteHeader" fontSet="material-icons-outlined">delete</mat-icon>
-          </button>
+
+        <div role="alert" *ngIf="showError && formControl.errors">
+          <formly-validation-message [field]="field"></formly-validation-message>
         </div>
-      </div>
-    </div>
-  <br/>`,
+
+        <mat-card-content *ngIf="field.fieldGroup.length != 0">
+          <div *ngFor="let field of field.fieldGroup; let i = index" class="row">
+            <div class="col-24">
+              <formly-field [field]="field"></formly-field>
+            </div>
+            <div *ngIf="field.props['removable'] !== false">
+              <button type="button" (click)="remove(i)" mat-flat-button>
+                <mat-icon fontSet="material-icons-outlined">delete</mat-icon>
+              </button>
+            </div>
+          </div>
+        </mat-card-content>
+      </mat-card>
+    </div>`,
   styleUrls: ['./array-type.scss']
 })
 export class ArrayTypeComponent extends FieldArrayType {
-  protected readonly style = style;
+
 }
