1b5271986c70686cc6e0318a105635059763e59a
Pratik Poudel <poudel@data-cybernetics.com>
2023-04-18 11:02:41 +0200

    glitch fix on formly-validation-message, minor style changes #853

--- src/app/hypermedia-view/actions-view/parameter-action/formly-types/array-type.ts
+++ src/app/hypermedia-view/actions-view/parameter-action/formly-types/array-type.ts
@@ -4,7 +4,7 @@ import {style} from '@angular/animations';
 
 @Component({selector: 'formly-array-type',
   template: `
-    <div style="margin: 0 0 20px 0;">
+    <div>
       <legend *ngIf="props.label">{{ props.label }}</legend>
       <p *ngIf="props.description">{{ props.description }}</p>
       <div class="d-flex flex-row-reverse" style="margin-bottom: 3px;">
@@ -15,21 +15,19 @@ import {style} from '@angular/animations';
       <div role="alert" *ngIf="showError && formControl.errors">
         <formly-validation-message [field]="field"></formly-validation-message>
       </div>
-      <div *ngFor="let field of field.fieldGroup; let i = index" class="row align-items-start">
-        <mat-grid-list cols="12" rowHeight="50px">
-          <mat-grid-tile colspan="11">
-            <formly-field style="width: 100%" [field]="field"></formly-field>
-          </mat-grid-tile>
-          <mat-grid-tile colspan="1">
-            <div *ngIf="field.props['removable'] !== false" class="col-2 text-right">
-              <button type="button" (click)="remove(i)" mat-flat-button>
-                <mat-icon class="deleteHeader" fontSet="material-icons-outlined">delete</mat-icon>
-              </button>
-            </div>
-          </mat-grid-tile>
-        </mat-grid-list>
+      <div *ngFor="let field of field.fieldGroup; let i = index" class="form-field-container">
+        <div class="col-10">
+          <formly-field [field]="field"></formly-field>
+        </div>
+        <div *ngIf="field.props['removable'] !== false" class="col-2">
+          <button type="button" (click)="remove(i)" mat-flat-button>
+            <mat-icon class="deleteHeader" fontSet="material-icons-outlined">delete</mat-icon>
+          </button>
+        </div>
       </div>
-    </div> `
+    </div>
+  <br/>`,
+  styleUrls: ['./array-type.scss']
 })
 export class ArrayTypeComponent extends FieldArrayType {
   protected readonly style = style;
