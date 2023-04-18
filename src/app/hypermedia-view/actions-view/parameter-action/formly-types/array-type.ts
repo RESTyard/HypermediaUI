ee7e8c0b4c0fe9d0ad8e3223a45f229de83bfd2e
Pratik Poudel <poudel@data-cybernetics.com>
2023-04-18 07:57:18 +0200

    css fix #853

--- src/app/hypermedia-view/actions-view/parameter-action/formly-types/array-type.ts
+++ src/app/hypermedia-view/actions-view/parameter-action/formly-types/array-type.ts
@@ -1,15 +1,18 @@
 import {Component} from '@angular/core';
 import {FieldArrayType} from '@ngx-formly/core';
+import {style} from '@angular/animations';
 
 @Component({selector: 'formly-array-type',
   template: `
     <div style="margin: 0 0 20px 0;">
       <legend *ngIf="props.label">{{ props.label }}</legend>
       <p *ngIf="props.description">{{ props.description }}</p>
-      <div class="d-flex flex-row-reverse">
-        <mat-icon id="addHeaderButton" (click)="add()">add</mat-icon>
+      <div class="d-flex flex-row-reverse" style="margin-bottom: 3px;">
+        <button type="button" (click)="add()" mat-flat-button>
+          <mat-icon id="addHeaderButton">add</mat-icon>
+        </button>
       </div>
-      <div class="alert alert-danger" role="alert" *ngIf="showError && formControl.errors">
+      <div role="alert" *ngIf="showError && formControl.errors">
         <formly-validation-message [field]="field"></formly-validation-message>
       </div>
       <div *ngFor="let field of field.fieldGroup; let i = index" class="row align-items-start">
@@ -19,12 +22,15 @@ import {FieldArrayType} from '@ngx-formly/core';
           </mat-grid-tile>
           <mat-grid-tile colspan="1">
             <div *ngIf="field.props['removable'] !== false" class="col-2 text-right">
-              <mat-icon class="deleteHeader" fontSet="material-icons-outlined" (click)="remove(i)">delete</mat-icon>
+              <button type="button" (click)="remove(i)" mat-flat-button>
+                <mat-icon class="deleteHeader" fontSet="material-icons-outlined">delete</mat-icon>
+              </button>
             </div>
           </mat-grid-tile>
         </mat-grid-list>
       </div>
-    </div> `,
+    </div> `
 })
 export class ArrayTypeComponent extends FieldArrayType {
+  protected readonly style = style;
 }
