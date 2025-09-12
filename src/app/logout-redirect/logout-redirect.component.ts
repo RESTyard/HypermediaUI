6c7f61e9bcc18c5edf58f69f9eea999be62a7a82
Moritz Vogel <moritz.vogel@bluehands.de>
2025-09-12 09:24:26 +0200

    feat: rework ui of logout-redirect component

--- src/app/logout-redirect/logout-redirect.component.ts
+++ src/app/logout-redirect/logout-redirect.component.ts
@@ -7,14 +7,15 @@ import {CurrentEntryPoint} from "../store/entrypoint.reducer";
 import {HypermediaClientService} from "../hypermedia-view/hypermedia-client.service";
 import {SettingsService} from "../settings/services/settings.service";
 import {combineLatest, filter, firstValueFrom} from "rxjs";
-import {isSuccess} from "fnxt/result";
+import {Failure, isSuccess, Result} from "fnxt/result";
 import {redirectToHuiPage} from "../utils/redirect";
 import {ApiPath} from "../hypermedia-view/api-path";
 import {MatToolbar} from "@angular/material/toolbar";
 import {SettingsModule} from "../settings/settings.module";
-import {MatCard} from "@angular/material/card";
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 import {MatButton} from "@angular/material/button";
 import { MatIconModule } from '@angular/material/icon';
+import {Unit} from "../utils/unit";
 
 @Component({
   selector: 'app-logout-redirect',
@@ -23,10 +24,14 @@ import { MatIconModule } from '@angular/material/icon';
     MatToolbar,
     SettingsModule,
     MatCard,
-    MatButton
+    MatButton,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatCardActions
   ],
   templateUrl: './logout-redirect.component.html',
-  styleUrl: './logout-redirect.component.css'
+  styleUrl: './logout-redirect.component.scss'
 })
 export class LogoutRedirectComponent implements OnInit {
   isSuccess: boolean = false;
@@ -57,9 +62,13 @@ export class LogoutRedirectComponent implements OnInit {
     this.configuredEntryPoint = configuredEntryPoints?.find(e => e.alias === path);
 
     const entryPoint = queryParams['entrypoint_uri'];
-    this.title = this.configuredEntryPoint?.title ?? "Hypermedia UI";
 
-    const logoutResult = await this.authService.handleLogoutCallback(entryPoint);
+    this.title = this.configuredEntryPoint?.title ?? "Hypermedia UI";
+    let logoutResult: Result<Unit, string> = Failure("No entrypoint");
+    if(entryPoint) {
+      logoutResult = await this.authService.handleLogoutCallback(entryPoint);
+    }
+
     if (isSuccess(logoutResult)) {
       this.isSuccess = true;
     } else {
