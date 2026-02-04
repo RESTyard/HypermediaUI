c7905b14d2865ceccb4b6c8a7e2712c9f7702097
Mathias Reichardt <reichardt@data-cybernetics.com>
2026-02-04 01:10:31 +0100

    Fix wrong siren mime type

--- src/app/hypermedia-view/MediaTypes.ts
+++ src/app/hypermedia-view/MediaTypes.ts
@@ -2,6 +2,6 @@ export class MediaTypes {
 
   public static Json = 'application/json';
   public static FormData = 'multipart/form-data';
-  public static Siren = 'application/siren+json';
+  public static Siren = 'application/vnd.siren+json';
   public static OctetStream = 'application/octet-stream';
 }
